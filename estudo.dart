32cfab9c84de0c5006c82cfbec194f189ef73d94
Alisson <125320258+alissongom@users.noreply.github.com>
2023-05-22 19:06:34 -0300

    att

--- estudo.dart
+++ estudo.dart
@@ -1,1 +1,25 @@
+void main() { // essa é a função que sempre inicia aplicação
+    print("hello wold");
+}
 
+# variaveis
+var primeiraVariavel = "Hello World";
+void main() {
+    primeiraVariavel = "Hello"; // mudando o valor da variavel
+    print(primeiraVariavel); // chamando o valor que tá na variavel
+}
+
+// bool True = 1; False = 0;
+void main() {
+    final a = "Gabul";
+    final b = "dev";
+    final isTrue = True;
+    final isFalse = False;
+    print(!isFalse ? "esta verdadeiro" : "esta falso")
+}
+
+// tipo int
+var meuInteiro; //tipo dinâmico
+var meuInteiro = 1; //adicionando o valor inicial
+int meuInteiro; //tipo explícito
+int meuInteiro = 1; //tipo explícito com valor inicial
